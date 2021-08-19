ae1dcdd365939572cc05e919309a75aa81fc55c9
HeltonCarlos <heltonbritopa@gmail.com>
2021-08-18 23:05:18 -0300

    header

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,20 @@
 <script>
 
 export default {  
-  
   data() {
     return{}
   },
 }
 </script>
 
-
+<style>
+*{padding: 0px;margin:0px;box-sizing: border-box;font-family: 'Oswald', sans-serif;}
+.flex{display: flex;}
+.flex-alinhar{display: flex;align-items: center;}
+.flex-coluna{display: flex;flex-direction: column;}
+.container-flex{max-width: 70%;display: flex;margin:0 auto; justify-content: space-between;}
+.flex-wrap{display:flex;flex-wrap: wrap;}
+a{text-decoration: none;}
+li{list-style: none;}
+</style>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Logar from '../views/Logar.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/Logar',
+    name: 'Logar',
+    component: Logar
   }
 ]
 
